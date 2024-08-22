09676c8ff6350b35b939b9333537839da4e4f9bb
Yueh-Shun Li <shamrocklee@posteo.net>
2024-08-22 15:25:22 +0800

    transcript-timestamper: fix Nix evaluation error on python 3.9

--- transcript-timestamper/python-packages-extension.nix
+++ transcript-timestamper/python-packages-extension.nix
@@ -14,6 +14,11 @@ in {
   # dragonmapper dependency
   hanzidentifier = callPythonModuleFromNixpkgs nixpkgs-dragonmapper "hanzidentifier" {};
 
+  # ipython-8.24.0 not supported for interpreter python3.9
+  python-dotenv = prev.python-dotenv.override (final.lib.optionalAttrs (final.pythonOlder "3.10") {
+    ipython = null;
+  });
+
   transcript-timestamper = final.callPackage ./package.nix {};
 
   # dragonmapper dependency
